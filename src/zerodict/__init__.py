e9ba3bde160a9a9e96512b0978ab9a7574ed8478
Francesco Favi <14098835+francescofavi@users.noreply.github.com>
2026-04-28 00:07:52 +0200

    chore: release 0.1.2

--- src/zerodict/__init__.py
+++ src/zerodict/__init__.py
@@ -14,7 +14,7 @@ License: MIT
 from zerodict.missing_path import MissingPath
 from zerodict.zerodict import ZeroDict
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Francesco Favi"
 __email__ = "14098835+francescofavi@users.noreply.github.com"
 
